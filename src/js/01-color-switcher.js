9727f1a1823e5beddfeeb28fe5526d6c90d52abb
Килимник Дмитро <dinakilimnik@gmail.com>
2022-10-29 02:48:47 +0300

    1/3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,45 @@
-const start = document.querySelector('button[data-start]');
+const btStart = document.querySelector('[data-start]');
+const btStop = document.querySelector('[data-stop]');
 
-start.addEventListener('click', funk);
+btStop.addEventListener('click', onBtStopClick);
+btStart.addEventListener('click', onBtStartClick);
 
-function funk(){
-    console.log('1');
+let tim = 0;
+
+function onBtStopClick(){
+    clearTimeout(tim);
+    onStop();
+};
+
+function onBtStartClick(){
+    onStart();
+    tim = setTimeout(() => {
+        onBtStartClick();
+         changeColor();
+    }, 2000);
 }
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  function changeColor(){
+    document.body.style.backgroundColor = getRandomHexColor();
+  }
+
+  function ifTrue() {
+    if(btStop.disabled === 'true'){
+        btStart.disabled = 'true';
+        btStop.removeAttribute('disabled');
+    }
+        btStop.disabled = 'true';
+    btStart.removeAttribute('disabled'); 
+  }
+  function onStart() {
+    btStart.disabled = 'true';
+    btStop.removeAttribute('disabled');
+  }
+  function onStop() {
+    btStop.disabled = 'true';
+    btStart.removeAttribute('disabled');
+  }
