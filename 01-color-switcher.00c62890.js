b273111c2dca142fccf68ce6a3c9703932cedcac
DimaKIL <104536061+DimaKIL@users.noreply.github.com>
2022-10-28 23:09:23 +0000

    Deploying to gh-pages from @ DimaKIL/goit-js-hw-09@f56b7aadf27b4f927c878f883e5967912967f9c3 🚀

---
+++ 01-color-switcher.00c62890.js
@@ -0,0 +1,2 @@
+document.querySelector("button[data-start]").addEventListener("click",(function(){console.log("1")}));
+//# sourceMappingURL=01-color-switcher.00c62890.js.map
---
+++ 01-color-switcher.32dff750.js
@@ -0,0 +1,2 @@
+document.querySelector("button[data-start]").addEventListener("click",(function(){console.log("1")}));
+//# sourceMappingURL=01-color-switcher.32dff750.js.map
